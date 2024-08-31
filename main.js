557f3039c32c1b2ec8603a0e7e6356af8ed1d7dc
malupadilla <145455169+malupadilla@users.noreply.github.com>
2024-08-31 20:11:00 -0300

    zoio

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
 let count = 0;
 
+// Verifica se existe um valor armazenado para 'count' no Local Storage
+if (localStorage.getItem('count')) {
+    count = parseInt(localStorage.getItem('count'));
+}
+
+// Atualiza o elemento HTML com o valor de 'count' ao carregar a página
 const countElement = document.getElementById('counter');
+countElement.textContent = `${count}`;
+
 const clickButton = document.getElementById('clickButton');
 const resetButton = document.getElementById('resetButton');
 
@@ -9,14 +17,15 @@ const resetButton = document.getElementById('resetButton');
 clickButton.addEventListener('click', () => {
     count++;
     countElement.textContent = `${count}`;
+    localStorage.setItem('count', count); // Armazena o valor atualizado no Local Storage
 });
 
 document.addEventListener("keydown", function (e) {
-    if ((e.keyCode === 32) ||  (e.key === "d") ||  (e.key === "f")) {
+    if ((e.keyCode === 32) || (e.key === "d") || (e.key === "f")) {
         count++;
         countElement.textContent = `${count}`; 
+        localStorage.setItem('count', count); // Armazena o valor atualizado no Local Storage
     }
-    
 });
 
 /* Clique de Reset */
@@ -24,4 +33,5 @@ document.addEventListener("keydown", function (e) {
 resetButton.addEventListener('click', () => {
     count = 0;
     countElement.textContent = `${count}`;
+    localStorage.setItem('count', count); // Armazena o valor zerado no Local Storage
 });
